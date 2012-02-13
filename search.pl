846b158748911aac0957e212bc1af8573f8621a3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-02-13 15:14:18 +0100

    First more or less operational test

--- search.pl
+++ search.pl
@@ -1,14 +1,12 @@
 :- module(search,
-	  [ bf_search/4,		% :Edge, ?Src, ?Dest, -Path
-	    bf_search/5			% :Edge, ?Witness, ?Src, ?Dest, -Path
+	  [ bf_expand/3			% :Edge, ?Src, -Path
 	  ]).
 :- use_module(library(assoc)).
 :- use_module(library(lists)).
 :- use_module(library(error)).
 
 :- meta_predicate
-	bf_search(2, ?, ?, -),
-	bf_search(2, ?, ?, ?, -).
+	bf_expand(2, +, -).
 
 /** <module> Search library
 
@@ -17,83 +15,52 @@ algorithms.
 
 */
 
-%%	bf_search(:Edge, ?Src, ?Dest, -Path) is nondet.
+%%	bf_expand(:Edge, ?Src, -RevPath) is nondet.
 %
 %	Find a path  through  a  graph  for   which  the  edges  can  be
 %	enumerated using call(Edge, From, To).
+%
+%	@param RevPath is the reversed path.  I.e., the head of this
+%	path is the end of the path.  Using this argument order makes
+%	it easier to find the target.
 
-bf_search(Edge, Src, Dest, Path) :-
+bf_expand(Edge, Src, Path) :-
 	nonvar(Src), !,
-	empty_assoc(Visited0),
-	put_assoc(Src, Visited0, true, Visited),
-	bf_search(Edge, [Src|AT], AT, Visited, forward, Dest, Path).
-bf_search(Edge, Src, Dest, Path) :-
-	nonvar(Dest), !,
-	empty_assoc(Visited0),
-	put_assoc(Dest, Visited0, true, Visited),
-	bf_search(Edge, [Dest|AT], AT, Visited, backward, Src, Path).
-bf_search(_, Src, Dest, _) :-
-	instantiation_error(Src-Dest).
-
-bf_search(_, Agenda, AT, _, _, _, _) :-
-	Agenda == AT, !, fail.
-bf_search(Edge, [Src|Agenda], AT, Visited, Dir, Dest, Path) :-
-	findall(New-Src, connected_unvisited(Dir, Edge, Src, Visited, New), NewList),
-	into_visited(NewList, Visited, NewVisited, NewList2, []),
-	(   Path = [Src,Dest],
-	    member(Dest-Src, NewList)
-	;   Path = [Src|Path2],
-	    strip_w_agenda(NewList2, AT, NAT),
-	    bf_search(Edge, Agenda, NAT, NewVisited, Dir, Dest, Path2)
+	(   Path = [Src]
+	;   empty_assoc(Visited0),
+	    put_assoc(Src, Visited0, [[Src]], Visited),
+	    bf_expand(Edge, [[Src]|AT], AT, Visited, forward, Path)
 	).
+bf_expand(_, Src, _) :-
+	instantiation_error(Src).
 
-connected_unvisited(forward, Edge, Src, Visited, New) :-
-	call(Edge, Src, New),
-	\+ get_assoc(New, Visited, _).
-connected_unvisited(backward, Edge, Src, Visited, New) :-
-	call(Edge, New, Src),
-	\+ get_assoc(New, Visited, _).
-
-into_visited([], Visited, Visited, T, T).
-into_visited([H|R], Visited0, Visited, T0, T) :-
-	get_assoc(H, Visited0, true), !,
-	into_visited(R, Visited0, Visited, T0, T).
-into_visited([H|R], Visited0, Visited, [H|T0], T) :-
-	put_assoc(H, Visited0, true, Visited1),
-	into_visited(R, Visited1, Visited, T0, T).
-
-
-%%	bf_search(:Edge, ?W, ?Src, ?Dest, -Path) is nondet.
-%
-%	Find a path  through  a  graph  for   which  the  edges  can  be
-%	enumerated using call(Edge, From, To).
-
-bf_search(Edge, W, Src, Dest, [n(Src)|Path]) :-
-	nonvar(Src), !,
-	empty_assoc(Visited0),
-	put_assoc(Src, Visited0, true, Visited),
-	bf_search(Edge, W, [Src|AT], AT, Visited, forward, Dest, Path).
-bf_search(Edge, W, Src, Dest, [n(Src)|Path]) :-
-	nonvar(Dest), !,
-	empty_assoc(Visited0),
-	put_assoc(Dest, Visited0, true, Visited),
-	bf_search(Edge, W, [Dest|AT], AT, Visited, backward, Src, Path).
-bf_search(_, _, Src, Dest, _) :-
-	instantiation_error(Src-Dest).
-
-bf_search(_, _, Agenda, AT, _, _, _, _) :-
+bf_expand(_, Agenda, AT, _, _, _) :-
 	Agenda == AT, !, fail.
-bf_search(Edge, W, [Src|Agenda], AT, Visited, Dir, Dest, Path) :-
-	findall(New-W, connected_unvisited(Dir, Edge, Src, Visited, New), NewList),
-	into_visited(NewList, Visited, NewVisited, NewList2, []),
-	(   Path = [w(W), n(Dest)],
-	    member(Dest-W, NewList)
+bf_expand(Edge, [P0|Agenda], AT, Visited, Dir, Path) :-
+	P0 = [Src|_],
+	findall(New, connected(Dir, Edge, Src, New), NewList),
+	into_visited(NewList, P0, Visited, NewVisited,
+		     NewPaths,
+		     NewList2, NewTail),
+	(   NewTail = [],
+	    member(Path, NewList2)
+	;   member(Path, NewPaths)
 	;   AT = NewList2,
-	    Path = [New|Path2],
-	    strip_w_agenda(NewList2, AT, NAT),
-	    bf_search(Edge, Agenda, NAT, NewVisited, Dir, Dest, Path2)
+	    NAT = NewTail,
+	    bf_expand(Edge, Agenda, NAT, NewVisited, Dir, Path)
 	).
 
-strip_w_agenda([], NAT, NAT).
-strip_w_agenda([N-_|R], [N|T0], T) :-
-	strip_w_agenda(R, T0, T).
+connected(forward, Edge, Src, New) :-
+	call(Edge, Src, New).
+connected(backward, Edge, Src, New) :-
+	call(Edge, New, Src).
+
+into_visited([], _, Visited, Visited, [], T, T).
+into_visited([H|R], Path, Visited0, Visited, [[H|Path]|NewPaths], T0, T) :-
+	get_assoc(H,
+		  Visited0, Paths,
+		  Visited1, [[H|Path]|Paths]), !,
+	into_visited(R, Path, Visited1, Visited, NewPaths, T0, T).
+into_visited([H|R], Path, Visited0, Visited, NewPaths, [[H|Path]|T0], T) :-
+	put_assoc(H, Visited0, [[H|Path]], Visited1),
+	into_visited(R, Path, Visited1, Visited, NewPaths, T0, T).
--- test_subprop.pl
+++ test_subprop.pl
@@ -1,5 +1,5 @@
 :- module(test_subprop,
-	  [ test/2			% +Size, +Steps
+	  [ test/0
 	  ]).
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(record)).
@@ -7,6 +7,7 @@
 :- use_module(library(debug)).
 :- use_module(library(broadcast)).
 :- use_module(random_graph).
+:- use_module(search).
 
 /** <module> Test RDF subproperty handling
 
@@ -23,105 +24,93 @@ are named p1, p2, ... pN.
 
 :- debug(subprop).
 
-:- meta_predicate
-	nth0_answer(+, 0).
-
-:- record
-	state(size:integer=20,
-	      min_connections:integer=0,
-	      max_connections:integer=40,
-	      duplicates:boolean=false,
-	      generation:integer=0,
-	      count:integer=0,
-	      direction=up).
-
 :- dynamic
-	generation/1,			% Current generation
 	predicate/2,			% Predicate, Gen
 	sub_of/3,			% P1, P2, Born
 	died/2.				% Born, Died
 
 cleanup :-
-	retractall(generation(_)),
 	retractall(predicate(_,_)),
 	retractall(sub_of(_,_,_)),
 	retractall(died(_,_)),
-	assertz(generation(1)).
+	rdf_reset_db.
+
+test :-
+	show,
+	reset,
+	loop(1).
+
+loop(I) :-
+	step,
+	check_all,
+	succ(I, I2),
+	format(user_error, '\r~t~D~6|', [I]),
+	loop(I2).
+
 
 :- listen(graph(Action), update_graph(Action)).
 
 update_graph(reset) :-
 	cleanup.
-update_graph(add_node(P)) :-
+update_graph(add_node(I)) :-
+	atom_concat(p, I, P),
 	rdf_assert(P,P,P),
 	rdf_generation(Gen),
 	assertz(predicate(P, Gen)).
-update_graph(add_edge(Sub,Super)) :-
+update_graph(add_edge(SubI,SuperI)) :-
+	atom_concat(p, SubI, Sub),
+	atom_concat(p, SuperI, Super),
 	rdf_assert(Sub, rdfs:subPropertyOf, Super),
 	rdf_generation(Gen),
 	assertz(sub_of(Sub,Super,Gen)).
-update_graph(del_edge(Sub,Super)) :-
+update_graph(del_edge(SubI,SuperI)) :-
+	atom_concat(p, SubI, Sub),
+	atom_concat(p, SuperI, Super),
 	rdf_retractall(Sub, rdfs:subPropertyOf, Super),
 	rdf_generation(Gen),
 	forall((sub_of(Sub,Super,Born),
 		\+ died(Born, _)),
 	       assertz(died(Born, Gen))).
 
-
-subPropertyOf_1(Sub, Super) :-
+check_all :-
 	rdf_generation(Gen),
-	subPropertyOf(Sub, Super, Gen).
+	check_all(Gen).
 
-subPropertyOf_1(Sub, Super, Gen) :-
+check_all(Gen) :-
+	forall(predicate(P1,_),
+	       forall(predicate(P2,_),
+		      check_all(Gen, P1, P2))).
+
+check_all(Gen, Sub, Super) :-
+	(   subPropertyOf(Gen, Sub, Super)
+	->  assertion(rdf_has(Sub, Super, Sub))
+	;   assertion(\+ rdf_has(Sub, Super, Sub))
+	).
+
+
+
+subPropertyOf_1(Gen, Sub, Super) :-
 	sub_of(Sub, Super, Born),
 	Gen >= Born,
 	\+ (  died(Born, Died),
 	      Gen >= Died
 	   ).
 
-%%	subPropertyOf(?Sub, ?Super, +Gen) is nondet.
+superPropertyOf_1(Gen, Super, Sub) :-
+	subPropertyOf_1(Gen, Sub, Super).
+
+
+%%	subPropertyOf(+Gen, ?Sub, ?Super) is nondet.
 %
 %	True when Node1 and Node2 are   connected, considering the graph
 %	as an undirected graph.
 
-subPropertyOf(Sub, Super, Gen) :-
+subPropertyOf(Gen, Sub, Super) :-
 	nonvar(Sub), nonvar(Super), !,
-	connected([Sub], [Sub], Super, Gen), !.
+	bf_expand(subPropertyOf_1(Gen), Sub, [Super|_]), !.
 subPropertyOf(Sub, Super, Gen) :-
 	nonvar(Sub), !,
-	connected([Sub], [Sub], Super, Gen).
-
-
-connected([Sub|Agenda], Visited, Super, Gen) :-
-	findall(New, connected_unvisited(Sub, Visited, New, Gen), NewList),
-	(   member(Node, NewList)
-	;   append(NewList, Visited, NewVisited),
-	    append(Agenda, NewList, NewAgenda),
-	    connected(NewAgenda, NewVisited, Node, Gen)
-	).
-
-connected_unvisited(N, Visited, New, Gen) :- !,
-	(   edge(N, New)
-	;   edge(New, N)
-	),
-	\+ memberchk(New, Visited).
-connected_unvisited(N, Visited, forward, New) :- !,
-	edge(N, New),
-	\+ memberchk(New, Visited).
-connected_unvisited(N, Visited, backward, New) :- !,
-	edge(New, N),
-	\+ memberchk(New, Visited).
-connected_unvisited(N, Visited, both_except(Child,Parent), New) :- !,
-	(   edge(N, New),
-	    \+ (N == Child, New == Parent)
-	;   edge(New, N),
-	    \+ (New == Child, N == Parent)
-	),
-	\+ memberchk(New, Visited).
-
-revert_dir(both,	     both).
-revert_dir(both_except(C,P), both_except(P,C)).
-revert_dir(forward,	     backward).
-revert_dir(backward,	     forward).
-
-
+	bf_expand(subPropertyOf_1(Gen), Sub, [Super|_]).
+subPropertyOf(Sub, Super, Gen) :-
+	nonvar(Super), !,
+	bf_expand(superPropertyOf_1(Gen), Super, [Sub|_]).
