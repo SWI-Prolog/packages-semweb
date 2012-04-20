bf53be858531e712fe359fc3e4909072750064f3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-04-20 13:14:49 +0200

    Introduce explicit graphs in test suite to prepare for concurrent tests

--- test_subprop.pl
+++ test_subprop.pl
@@ -26,119 +26,127 @@ are named p1, p2, ... pN.
 :- debug(subprop).
 
 :- dynamic
-	predicate/2,			% Predicate, Gen
-	sub_of/3,			% P1, P2, Born
-	snap/3,				% SnapID, Gen, Snap
-	died/2.				% Born, Died
+	predicate/3,			% Predicate, Gen, Graph
+	sub_of/4,			% P1, P2, Born, Graph
+	snap/4,				% SnapID, Gen, Snap, Graph
+	died/3.				% Born, Died, Graph
 
-cleanup :-
-	retractall(predicate(_,_)),
-	retractall(sub_of(_,_,_)),
-	retractall(died(_,_)),
-	retractall(snap(_,_,_)),
+cleanup(G) :-
+	retractall(predicate(_,_,G)),
+	retractall(sub_of(_,_,_,G)),
+	retractall(died(_,_,G)),
+	retractall(snap(_,_,_,G)),
 %	rdf_gc,
 	rdf_reset_db.
 
 test(N) :-
-	record_in('g1.rec'),
-	show_graph(g1),
-	graph_settings(g1,
+	test(g1, N).
+
+test(G, N) :-
+	atom_concat(G, '.rec', LogFile),
+	setup_call_cleanup(
+	    record_in(LogFile, LogStream),
+	    run_test(G, N),
+	    close_recording(LogStream)).
+
+run_test(G, N) :-
+	show_graph(G),
+	graph_settings(G,
 		       [ verify(0.01),
 			 create_snap(0.1),
 			 verify_snap(0.01),
 			 delete_snap(0.05)
 		       ]),
-	reset_graph(g1),
-	loop(1, N).
+	reset_graph(G),
+	setup_call_cleanup(
+	    listen(G, graph(G,Action), update_graph_true(G, Action)),
+	    loop(1, N, G),
+	    unlisten(G)).
 
-loop(I, I) :- !.
-loop(I, N) :-
-	graph_steps(g1,1),
+loop(I, I, _) :- !.
+loop(I, N, G) :-
+	graph_steps(G,1),
 	succ(I, I2),
 	format(user_error, '\r~t~D~6|', [I]),
-	loop(I2, N).
+	loop(I2, N, G).
 
 
-:- listen(graph(g1, Action), update_graph_true(Action)).
-
-update_graph_true(Action) :-
-	update_graph(Action), !.
-update_graph_true(_Action) :-
+update_graph_true(G, Action) :-
+	debug(subprop, '~q: ~p', [G, Action]),
+	update_graph(G, Action), !.
+update_graph_true(_, _Action) :-
 	assertion(false).
 
-update_graph(Action) :-
-	debug(subprop, '~p', [Action]),
-	fail.
-update_graph(reset) :-
-	cleanup.
-update_graph(verify) :-
-	check_all.
-update_graph(create_snap(SnapId)) :-
+update_graph(G, reset) :-
+	cleanup(G).
+update_graph(G, verify) :-
+	check_all(G).
+update_graph(G, create_snap(SnapId)) :-
 	rdf_snapshot(Snap),
 	rdf_generation(Gen),
-	assertz(snap(SnapId, Gen, Snap)).
-update_graph(verify_snap(SnapId)) :-
-	snap(SnapId, Gen, Snap),
+	assertz(snap(SnapId, Gen, Snap, G)).
+update_graph(G, verify_snap(SnapId)) :-
+	snap(SnapId, Gen, Snap, G),
 	rdf_transaction(check_all(Gen), _Id, [snapshot(Snap)]).
-update_graph(delete_snap(SnapId)) :-
-	retract(snap(SnapId, _Gen, Snap)),
+update_graph(G, delete_snap(SnapId)) :-
+	retract(snap(SnapId, _Gen, Snap, G)),
 	rdf_delete_snapshot(Snap).
-update_graph(add_node(I)) :-
+update_graph(G, add_node(I)) :-
 	atom_concat(p, I, P),
 	rdf_statistics(triples(T0)),
 	rdf_assert(P,P,P),
 	rdf_statistics(triples(T1)),
 	assertion(T0+1 =:= T1),
 	rdf_generation(Gen),
-	assertz(predicate(P, Gen)).
-update_graph(add_edge(SubI,SuperI)) :-
+	assertz(predicate(P, Gen, G)).
+update_graph(G, add_edge(SubI,SuperI)) :-
 	atom_concat(p, SubI, Sub),
 	atom_concat(p, SuperI, Super),
 	rdf_assert(Sub, rdfs:subPropertyOf, Super),
 	rdf_generation(Gen),
-	assertz(sub_of(Sub,Super,Gen)).
-update_graph(del_edge(SubI,SuperI)) :-
+	assertz(sub_of(Sub,Super,Gen,G)).
+update_graph(G, del_edge(SubI,SuperI)) :-
 	atom_concat(p, SubI, Sub),
 	atom_concat(p, SuperI, Super),
 	rdf_retractall(Sub, rdfs:subPropertyOf, Super),
 	rdf_generation(Gen),
-	forall((sub_of(Sub,Super,Born),
-		\+ died(Born, _)),
-	       assertz(died(Born, Gen))).
+	forall((sub_of(Sub,Super,Born,G),
+		\+ died(Born, _, G)),
+	       assertz(died(Born, Gen,G))).
 
-check_all :-
+check_all(Graph) :-
 	rdf_generation(Gen),
-	check_all(Gen).
+	check_all(Graph, Gen).
 
-check_all(Gen) :-
-	forall(visible_predicate(Gen, P1),
-	       forall(visible_predicate(Gen, P2),
-		      check_all(Gen, P1, P2))).
+check_all(Graph, Gen) :-
+	forall(visible_predicate(Graph, Gen, P1),
+	       forall(visible_predicate(Graph, Gen, P2),
+		      check_all(Graph, Gen, P1, P2))).
 
-check_all(Gen, Sub, Super) :-
-	(   subPropertyOf(Gen, Sub, Super)
+check_all(Graph, Gen, Sub, Super) :-
+	(   subPropertyOf(Graph, Gen, Sub, Super)
 	->  assertion(rdf_has(Sub, Super, Sub))
 	;   assertion(\+ rdf_has(Sub, Super, Sub))
 	).
 
 
 
-visible_predicate(Gen, P) :-
-	predicate(P, Born),
+visible_predicate(Graph, Gen, P) :-
+	predicate(P, Born, Graph),
 	Gen >= Born,
-	\+ (  died(Born, Died),
+	\+ (  died(Born, Died, Graph),
 	      Gen >= Died
 	   ).
 
-subPropertyOf_1(Gen, Sub, Super) :-
-	sub_of(Sub, Super, Born),
+subPropertyOf_1(Graph, Gen, Sub, Super) :-
+	sub_of(Sub, Super, Born, Graph),
 	Gen >= Born,
-	\+ (  died(Born, Died),
+	\+ (  died(Born, Died, Graph),
 	      Gen >= Died
 	   ).
 
-superPropertyOf_1(Gen, Super, Sub) :-
-	subPropertyOf_1(Gen, Sub, Super).
+superPropertyOf_1(Graph, Gen, Super, Sub) :-
+	subPropertyOf_1(Graph, Gen, Sub, Super).
 
 
 %%	subPropertyOf(+Gen, ?Sub, ?Super) is nondet.
@@ -146,15 +154,15 @@ superPropertyOf_1(Gen, Super, Sub) :-
 %	True when Node1 and Node2 are   connected, considering the graph
 %	as an undirected graph.
 
-subPropertyOf(Gen, Sub, Super) :-
+subPropertyOf(Graph, Gen, Sub, Super) :-
 	nonvar(Sub), nonvar(Super), !,
-	bf_expand(subPropertyOf_1(Gen), Sub, [Super|_]), !.
-subPropertyOf(Sub, Super, Gen) :-
+	bf_expand(subPropertyOf_1(Graph, Gen), Sub, [Super|_]), !.
+subPropertyOf(Graph, Sub, Super, Gen) :-
 	nonvar(Sub), !,
-	bf_expand(subPropertyOf_1(Gen), Sub, [Super|_]).
-subPropertyOf(Sub, Super, Gen) :-
+	bf_expand(subPropertyOf_1(Graph, Gen), Sub, [Super|_]).
+subPropertyOf(Graph, Sub, Super, Gen) :-
 	nonvar(Super), !,
-	bf_expand(superPropertyOf_1(Gen), Super, [Sub|_]).
+	bf_expand(superPropertyOf_1(Graph, Gen), Super, [Sub|_]).
 
 
 		 /*******************************
@@ -164,13 +172,14 @@ subPropertyOf(Sub, Super, Gen) :-
 :- dynamic
 	record_stream/1.
 
-record_in(File) :-
+record_in(File, Out) :-
 	open(File, write, Out),
 	asserta(record_stream(Out)),
-	listen(record, graph(G, Action), save(G, Action)),
-	at_halt(close_recording).
+	listen(record, graph(G, Action), save(G, Action)).
 
-close_recording :-
+:- at_halt(close_recording(_)).
+
+close_recording(Out) :-
 	forall(retract(record_stream(Out)),
 	       close(Out)).
 
@@ -184,9 +193,10 @@ save(Graph, Action) :-
 %	Replay the last randomly generated suite
 
 replay :-
-	replay('g1.rec').
+	replay(g1).
 
-replay(File) :-
+replay(Graph) :-
+	file_name_extension(Graph, rec, File),
 	open(File, read, In),
 	repeat,
 	    read(In, Term),
@@ -194,6 +204,6 @@ replay(File) :-
 	    ->	!, close(In)
 	    ;	Term = action(Graph,Action),
 		broadcast(graph(Graph, Action)),
-		check_all,
+		check_all(Graph),
 		fail
 	    ).
