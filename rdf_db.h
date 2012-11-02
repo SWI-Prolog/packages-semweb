4795c742478d391fe6f169b90941715c385c6c02
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-11-02 11:42:59 +0100

    Updated comments and layout

--- rdf_db.h
+++ rdf_db.h
@@ -1,11 +1,10 @@
-/*  $Id$
-
-    Part of SWI-Prolog
+/*  Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        wielemak@science.uva.nl
+    E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2007, University of Amsterdam
+    Copyright (C): 1985-2012, University of Amsterdam
+			      VU University Amsterdam
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -30,7 +29,7 @@
 #endif
 #include "lock.h"
 
-#define RDF_VERSION 20900		/* 2.8.0 */
+#define RDF_VERSION 20900		/* 2.9.0 */
 
 #define URL_subPropertyOf \
 	"http://www.w3.org/2000/01/rdf-schema#subPropertyOf"
@@ -69,8 +68,8 @@
 #define BY_SPOG	(BY_S|BY_P|BY_O|BY_G)	/* 15 */
 
 /* (*) INDEX_TABLES must be consistent with index_col[] in rdf_db.c */
-#define INDEX_TABLES 		        10 	/* (*)  */
-#define INITIAL_TABLE_SIZE   		1024
+#define INDEX_TABLES		        10	/* (*)  */
+#define INITIAL_TABLE_SIZE		1024
 #define INITIAL_PREDICATE_TABLE_SIZE	1024
 #define INITIAL_GRAPH_TABLE_SIZE	64
 
@@ -110,10 +109,10 @@ typedef struct predicate
   int		    label;		/* Numeric label in cloud */
   struct predicate_cloud *cloud;	/* cloud I belong to */
   size_t	    hash;		/* key used for hashing
-  					   (=hash if ->cloud is up-to-date) */
+					   (=hash if ->cloud is up-to-date) */
 					/* properties */
   struct predicate *inverse_of;		/* my inverse predicate */
-  unsigned 	    transitive : 1;	/* P(a,b)&P(b,c) --> P(a,c) */
+  unsigned	    transitive : 1;	/* P(a,b)&P(b,c) --> P(a,c) */
 					/* statistics */
   size_t	    triple_count;	/* # triples on this predicate */
   size_t	    distinct_updated[2];/* Is count still valid? */
@@ -141,7 +140,7 @@ typedef struct graph
   int		    triple_count;	/* # triples associated to it */
 #ifdef WITH_MD5
   unsigned	    md5 : 1;		/* do/don't record MD5 */
-  md5_byte_t 	    digest[16];		/* MD5 digest */
+  md5_byte_t	    digest[16];		/* MD5 digest */
 #endif
 } graph;
 
@@ -184,7 +183,7 @@ typedef struct triple
 					/* indexing */
   union
   { struct triple*next[INDEX_TABLES];	/* hash-table next links */
-    literal 	end;			/* end for between(X,Y) patterns */
+    literal	end;			/* end for between(X,Y) patterns */
   } tp;					/* triple or pattern */
 					/* flags */
   unsigned	object_is_literal : 1;	/* Object is a literal */
@@ -221,7 +220,7 @@ typedef struct transaction_record
   tr_type			type;
   triple		       *triple;		/* new/deleted triple */
   union
-  { triple		       *triple; 	/* used for update */
+  { triple		       *triple;		/* used for update */
     struct
     { atom_t			atom;
       unsigned long		line;
@@ -266,7 +265,7 @@ typedef struct rdf_db
   size_t	duplicates;		/* #duplicate triples */
   size_t	generation;		/* generation-id of the database */
   graph       **graph_table;		/* Hash table of sources */
-  int      	graph_table_size;	/* Entries in table */
+  int		graph_table_size;	/* Entries in table */
   int		graph_count;
 
   graph	*last_graph;		/* last accessed graph */
@@ -275,7 +274,7 @@ typedef struct rdf_db
   transaction_record *tr_last;		/* last transaction record */
   int		tr_nesting;		/* nesting depth of transactions */
   int		tr_reset;		/* transaction contains reset */
-  int 		resetting;		/* We are in rdf_reset_db() */
+  int		resetting;		/* We are in rdf_reset_db() */
 
   rwlock	lock;			/* threaded access */
 
