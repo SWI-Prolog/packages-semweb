SWI-Prolog/packages-semweb

commit 3e6c627770c45eb652de36c1a2d293604985c64d

FIXED: Possibility that free_search_state() is called twice.

rdf_db.c
@@ -7130,9 +7130,7 @@ rdf(term_t subject, term_t predicate, term_t object,
       state->dup_answers.entries = NULL;	/* see add_tripleset() */
 
       if ( !init_search_state(state, q) )
-      { free_search_state(state);
 	return FALSE;
-      }
 
       goto search;
     }

commit c63eeafbceb2c0b49d756671671a9b86d3d43380

FIXED: Failed to deallocate query if rdf/3 and friends are called from an illegal triple.  Bug introduced in 3e6c627770c45eb652de36c1a2d293604985c64d. Jacco van Ossenbruggen.

rdf_db.c
@@ -6749,7 +6749,7 @@ init_search_state(search_state *state, query *query)
   if ( get_partial_triple(state->db,
 			  state->subject, state->predicate, state->object,
 			  state->src, p) != TRUE )
-  { free_triple(state->db, p, FALSE);
+  { free_search_state(state);
     return FALSE;
   }
 