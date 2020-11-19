SWI-Prolog/packages-semweb

commit ef7ee735df9445b061cecc23c5d707d51e470edd

FIXED: Issue#99: preserve white space in SPARQL XML results.

sparql_client.pl
@@ -393,8 +393,7 @@ term_expansion(T0, T) :-
 
 sparql_read_xml_result(Input, Result) :-
     load_structure(Input, DOM,
-                   [ dialect(xmlns),
-                     space(remove)
+                   [ dialect(xmlns)
                    ]),
     call_cleanup(dom_to_result(DOM, Result),
                  retractall(bnode_map(_,_))).
@@ -426,13 +425,22 @@ variables([element(sparql:variable, Att, [])|T0], [Name|T]) :-
     memberchk(name=Name, Att),
     variables(T0, T).
 variables([element(sparql:link, _, _)|T0], T) :-
+    !,
+    variables(T0, T).
+variables([CDATA|T0], T) :-
+    atomic(CDATA),
     variables(T0, T).
 
 
 rows([], _, []).
 rows([R|T0], Vars, [Row|T]) :-
+    R = element(sparql:result, _, _),
+    !,
     row_values(Vars, R, Values),
     Row =.. [row|Values],
+    rows(T0, Vars, T).
+rows([CDATA|T0], Vars, T) :-
+    atomic(CDATA),
     rows(T0, Vars, T).
 
 row_values([], _, []).

commit cf5c8abe249ac2469f82f225c174171a12961ade

FIXED: sparql_query/3: ignore white space in values.  Johan Romme.
Bug introduced by ef7ee735df9445b061cecc23c5d707d51e470edd

sparql_client.pl
@@ -477,8 +477,9 @@ row_values([Var|VarT], DOM, [Value|ValueT]) :-
     ),
     row_values(VarT, DOM, ValueT).
 
-value([element(sparql:literal, Att, Content)], literal(Lit)) :-
+value([element(sparql:literal, Att, Content)|Rest], literal(Lit)) :-
     !,
+    white(Rest),
     lit_value(Content, Value),
     (   memberchk(datatype=Type, Att)
     ->  Lit = type(Type, Value)
@@ -486,12 +487,24 @@ value([element(sparql:literal, Att, Content)], literal(Lit)) :-
     ->  Lit = lang(Lang, Value)
     ;   Lit = Value
     ).
-value([element(sparql:uri, [], [URI])], URI) :- !.
-value([element(sparql:bnode, [], [NodeID])], URI) :-
+value([element(sparql:uri, [], [URI])|Rest], URI) :- !,
+    white(Rest).
+value([element(sparql:bnode, [], [NodeID])|Rest], URI) :-
     !,
+    white(Rest),
     bnode(NodeID, URI).
-value([element(sparql:unbound, [], [])], '$null$').
+value([element(sparql:unbound, [], [])|Rest], '$null$') :-
+    !,
+    white(Rest).
+value([CDATA|Rest], Value) :-
+    atomic(CDATA),
+    value(Rest, Value).
 
+
+white([]).
+white([CDATA|T]) :-
+    atomic(CDATA),
+    white(T).
 
 lit_value([], '').
 lit_value([Value], Value).